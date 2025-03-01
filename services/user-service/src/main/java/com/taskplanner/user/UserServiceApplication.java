71cb9c96cdd7a4ce1d0184d651f8efdaa9bcc5b1
Mihai Belea <mihaibelea@Anghels-iMac.local>
2025-03-01 22:42:20 +0200

    now it works

--- services/user-service/src/main/java/com/taskplanner/user/UserServiceApplication.java
+++ services/user-service/src/main/java/com/taskplanner/user/UserServiceApplication.java
@@ -3,7 +3,12 @@ package com.taskplanner.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+    exclude = { 
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+        org.springframework.boot.autoconfigure.security.servlet.SecurityFilterAutoConfiguration.class
+    }
+)
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
